3d613e25d40cee74608230e4f68d8b349872c2fd
alekzander86 <nicalib19@gmail.com>
2018-12-22 10:56:40 -0600

    primer commit

---
+++ precache-manifest.2308d98b95789dce63d2a06157d4e77f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "83520c1dbcd34f9c1712",
+    "url": "/static/js/main.83520c1d.chunk.js"
+  },
+  {
+    "revision": "4d4949a9a9f903a5cc4a",
+    "url": "/static/js/1.4d4949a9.chunk.js"
+  },
+  {
+    "revision": "83520c1dbcd34f9c1712",
+    "url": "/static/css/main.62f62823.chunk.css"
+  },
+  {
+    "revision": "d0172917d37d88ebbddc0ab6cb141a53",
+    "url": "/index.html"
+  }
+];
